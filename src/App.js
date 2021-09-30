ed4597d5a4542b37e60c6bff74005a3c3428bc8c
Tetracontadigon <87103643+Tetracontadigon@users.noreply.github.com>
2021-09-30 14:09:03 -0400

    inclass930

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import './App.css';
-
-
+import Axios from "axios"
+import { useState } from 'react';
 
 const App = () =>{
-
- return (
-
- <div className="container">
-
- <p>mr-headrick</p>
-
- </div>
-
- );
-
+    const [joke, setJoke] = useState()
+    const getJoke = () => {
+        Axios.get("https://icanhazdadjoke.com/", {
+            headers: {
+              "Accept": "application/json"
+            }
+          }
+    ).then(
+        (response) => {
+            //console.log(response);
+            setJoke(response.data.joke);
+        }
+    );
+};
+return (
+    <div className="container"><button onClick={getJoke}> Dad Joke </button>
+        <br/><p>{joke}</p>
+    </div>
+    );
 }
-
-
-
 export default App;
